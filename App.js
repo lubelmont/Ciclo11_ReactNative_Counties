642841ff2463f011f0c023fb7deeaed53c15c696
Arnufo Rodríguez <luis.rodriguez@lubelsoft.com>
2025-03-31 21:21:21 -0600

    Ne message

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-const titulo = "Hola Mundo";
+const titulo = "Hola Mundo desde React Native";
 
 const icon = require('./assets/icon.png');
 
